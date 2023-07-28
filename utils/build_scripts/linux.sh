ae2dc0ff0fc8b23b0fffa98505b3af731db3903b
MrKris7100 <czacha55@vp.pl>
2023-07-28 09:46:57 +0200

    Build script for linux
    Autonomous build script for morelo.
    Including building cmake and boost

---
+++ utils/build_scripts/linux.sh
@@ -0,0 +1,30 @@
+#Morelo build script for linux
+#Copyrights Morelo Network 2023
+
+#Install dependences
+sudo apt update && sudo apt install --yes git build-essential curl pkg-config libssl-dev libsodium-dev libunwind-dev liblzma-dev libreadline-dev libldns-dev libexpat1-dev doxygen graphviz libudev-dev libusb-1.0-0-dev libhidapi-dev xsltproc gperf autoconf automake libtool-bin
+
+#Build and install cmake 3.17.3 from source
+wget https://github.com/Kitware/CMake/releases/download/v3.17.3/cmake-3.17.3.tar.gz
+tar -xvf cmake-3.17.3.tar.gz
+cd cmake-3.17.3
+./bootstrap
+make -j4
+sudo make install
+cd ..
+
+#Build and install boost 1.73.0 from source
+wget https://boostorg.jfrog.io/artifactory/main/release/1.73.0/source/boost_1_73_0.tar.gz
+tar -xvf boost_1_73_0.tar.gz
+cd boost_1_73_0
+./bootstrap.sh
+./b2 -j4
+sudo ./b2 install
+sudo ./b2 headers
+cd ..
+
+#Build morelo from source
+git clone https://github.com/MoreloNetwork/morelo
+cd morelo
+cmake .
+make -j4
